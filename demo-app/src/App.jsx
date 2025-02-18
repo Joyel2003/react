976ae959f699febb100078e38d8fe192b4f4fc59
Joyel <joyelmathew806@gmail.com>
2025-02-18 09:36:27 +0530

    lines added

---
+++ demo-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { DOM_KEY_LOCATION } from '@testing-library/user-event/dist/keyboard/types';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React</h1>
+
+    </div>
+  );
+}
+
+export default App;
+
+joyel mathew
